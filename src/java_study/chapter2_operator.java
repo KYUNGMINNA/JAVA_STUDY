e42c689debcb485255c6936434361ef5a2658e13
kyungmin <nkm4095@naver.com>
2022-01-11 20:43:05 +0900

    2장 연산자

---
+++ src/java_study/chapter2_operator.java
@@ -0,0 +1,32 @@
+package java_study;
+
+public class chapter2_operator {
+    public static void main(String[] args) {
+
+        int a=10,b=3;
+
+        //산술 연산자
+        System.out.println("산술 연산자");
+        System.out.println("a: "+a+" "+"b: "+b);
+        System.out.println("a+b "+(a+b));
+        System.out.println("a-b "+(a-b));
+        System.out.println("a*b "+(a*b));
+        System.out.println("a/b "+(a/b));
+        System.out.println("a%b "+(a%b));
+        System.out.println("a<<b "+(a<<b));
+        System.out.println("a>>b "+(a>>b));
+
+        //비교 연산자
+        System.out.println("비교 연산자");
+        System.out.println("a: "+a+" "+"b: "+b);
+        System.out.println("a>b "+(a>b));
+        System.out.println("a<b "+(a<b));
+        System.out.println("a>=b "+(a>=b));
+        System.out.println("a<=b "+(a<=b));
+        System.out.println("a==b "+(a==b));
+        System.out.println("a!=b "+(a!=b));
+
+
+
+    }
+}

50cc5ceacdf2d4c9d03450928809e6a2c7e2930a
kyungmin <nkm4095@naver.com>
2022-01-11 20:55:48 +0900

    2장 연산자

--- src/java_study/chapter2_operator.java
+++ src/java_study/chapter2_operator.java
@@ -26,6 +26,13 @@ public class chapter2_operator {
         System.out.println("a==b "+(a==b));
         System.out.println("a!=b "+(a!=b));
 
+        //문자열 비교
+        System.out.println("문자열 비교 ");
+        String monja="asd";
+        boolean as=monja.equals("asd");
+        System.out.println(as);
+        System.out.println(!as);
+
         //논리 연산자
         System.out.println("논리 연산자");
         System.out.println("  x     y     &&     ||");
@@ -41,6 +48,9 @@ public class chapter2_operator {
         System.out.println("전위형 -- 참조 전 증가");
         System.out.println(" a=10 ++a "+(++a));
         System.out.println(" a=10 --a "+(--a));
+
+        //삼항 연산자
+        System.out.println("a=10 b=3  a>b?a:b  "+((a>b)?a:b));
 
 
     }

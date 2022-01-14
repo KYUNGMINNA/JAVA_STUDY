28cf3dc145aa79cf7d79c2d22f5a0be06de89a5f
kyungmin <nkm4095@naver.com>
2022-01-14 21:23:10 +0900

    5장 배열

---
+++ src/java_study/chapter5_array.java
@@ -0,0 +1,16 @@
+package java_study;
+
+public class chapter5_array {
+    public static void main(String[] args) {
+
+        //배열의 선언
+        int[] a;
+        int b[];
+
+        //배열의 생성--길이 지정해야함 
+        int[] a1=new int[1];
+        int b1[]=new int[2];
+
+
+    }
+}

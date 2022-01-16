f2a7c3c73820b7c81ac72a28b94fb43266566865
kyungmin <nkm4095@naver.com>
2022-01-16 21:16:00 +0900

    ch2_1

---
+++ src/chapter2/ch2_1.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class ch2_1 {
+    public static void main(String[] args) {
+        int year=0;
+        int age=14;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        System.out.println("year ,age 산술 연산자 적용");
+        year=age+2000;
+        age=age+1;
+
+        System.out.println(year);
+        System.out.println(age);
+    }
+}

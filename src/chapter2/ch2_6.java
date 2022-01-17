99b2b4fdabbb2b157282d83fdd116d2669b00e57
kyungmin <nkm4095@naver.com>
2022-01-17 11:16:31 +0900

    ch2

---
+++ src/chapter2/ch2_6.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+import java.util.*;
+public class ch2_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("두자리 정수를 입력해 주세요:");
+        String input=scanner.nextLine();
+        int num=Integer.parseInt(input);
+        System.out.println("입력 내용(문자열):"+input);
+        System.out.printf("입력한 내용을 정수로 변환 -- number=%d",num);
+
+
+    }
+
+}
---
+++ src/chapter2/ch2_8.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class ch2_8 {
+
+    public static void main(String[] args) {
+        System.out.println("\'");
+        System.out.println("\"");
+        System.out.println("abc\t123123\b456");
+        System.out.println("\n");
+        System.out.println("\\'");
+
+
+
+    }
+}

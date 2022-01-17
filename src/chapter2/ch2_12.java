5d3f7cf14f6abfdd3b1bd44abe6bc659d05da80d
kyungmin <nkm4095@naver.com>
2022-01-17 11:28:14 +0900

    ch2

---
+++ src/chapter2/ch2_12.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+public class ch2_12 {
+    public static void main(String[] args) {
+        double d=85.4;
+        int score=(int)d;
+        System.out.println("Score="+score);
+        System.out.println("d="+d);
+    }
+}
---
+++ src/chapter2/ch2_15.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+public class ch2_15 {
+    public static void main(String[] args) {
+        int i=91234567;
+        float f=(float)i;
+        int i2=(int)f;
+
+        double d=(double) i;
+        int i3=(int) d;
+
+        float f2=1.666f;
+        int i4=(int) f2;
+
+        System.out.printf("i=%d%n",i);
+        System.out.printf("f=%f,i=%d%n",f,i2);
+        System.out.printf("d=%f,i=%d%n",d,i3);
+        System.out.printf("f=%f,i=%d%n",f2,i4);
+    }
+}

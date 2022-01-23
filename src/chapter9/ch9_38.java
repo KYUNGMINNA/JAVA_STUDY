ec192c1921c575f10396a115374c4734701c2ce4
kyungmin <nkm4095@naver.com>
2022-01-23 20:39:00 +0900

    ch2

---
+++ src/chapter9/ch9_38.java
@@ -0,0 +1,13 @@
+package chapter9;
+
+import java.util.*;
+public class ch9_38 {
+    public static void main(String[] args) {
+        String source="100,200,300,400";
+        StringTokenizer st=new StringTokenizer(source,",");
+
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+    }
+}

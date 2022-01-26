877b23368bb071f29c1e1353548175c37f694c13
kyungmin <nkm4095@naver.com>
2022-01-26 10:59:12 +0900

    ch5

---
+++ src/chapter5/ch5_06.java
@@ -0,0 +1,22 @@
+package chapter5;
+
+public class ch5_06 {
+    public static void main(String[] args) {
+        int[] score={79,88,91,33,100,55,95};
+
+        int max=score[0];
+        int min=score[0];
+
+        for (int i=1;i<score.length;i++){
+            if(score[i]>max){
+                max=score[i];
+            }else if (score[i]<min){
+                min=score[i];
+
+            }
+        }
+        System.out.println("max :"+max);
+        System.out.println("min :"+min);
+    }
+
+}

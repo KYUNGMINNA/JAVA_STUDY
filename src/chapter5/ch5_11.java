f1d306105704c207e925ea16311e470b097e3666
kyungmin <nkm4095@naver.com>
2022-01-26 12:16:44 +0900

    ch5

---
+++ src/chapter5/ch5_11.java
@@ -0,0 +1,19 @@
+package chapter5;
+
+public class ch5_11 {
+    public static void main(String[] args) {
+        int[] numarr=new int[10];
+        int[] counter=new int[10];
+
+        for (int i=0;i<numarr.length;i++){
+            numarr[i]=(int) (Math.random()*10);
+            System.out.print(numarr[i]);
+        }
+        System.out.println();
+
+        for (int i=0;i<numarr.length;i++) counter[numarr[i]]++;
+
+        for (int i=0;i<numarr.length;i++) System.out.println(i+"의 개수 :"+counter[i]);
+
+    }
+}

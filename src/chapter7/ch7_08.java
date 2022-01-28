bf34873b3da031fac1c8a82940ad57f591e3f508
kyungmin <nkm4095@naver.com>
2022-01-28 14:09:16 +0900

    ch7

---
+++ src/chapter7/ch7_08.java
@@ -0,0 +1,8 @@
+package chapter7;
+
+public class ch7_08 {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/chapter7/ch7_09.java
@@ -0,0 +1,12 @@
+
+package chapter7;
+
+
+public class ch7_09 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello,world");
+
+    }
+
+}

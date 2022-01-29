02df55ceb9f384b6defcf4d958159bc316269d3a
kyungmin <nkm4095@naver.com>
2022-01-29 17:33:41 +0900

    ch7

---
+++ src/chapter7/ch7_15.java
@@ -0,0 +1,33 @@
+package chapter7;
+
+public class ch7_15 {
+
+    public static void main(String[] args) {
+
+        Car15 car=null;
+        FireEngine15 fe=new FireEngine15();
+        FireEngine15 f2=null;
+
+        fe.water();
+        car=fe;
+        f2=(FireEngine15) car;
+        f2.water();
+    }
+}
+class Car15{
+    String color;
+    int door;
+
+    void drive(){
+        System.out.println("drive ,Brrr~~");
+    }
+    void stop(){
+        System.out.println("stop!");
+    }
+
+}
+class FireEngine15 extends Car15{
+    void water(){
+        System.out.println("water!");
+    }
+}

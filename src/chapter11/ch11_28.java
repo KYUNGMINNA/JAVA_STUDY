57092adfdb0a6389ffa665edd16a6cd6c226d029
kyungmin <nkm4095@naver.com>
2022-02-08 22:28:04 +0900

    ch11

---
+++ src/chapter11/ch11_28.java
@@ -0,0 +1,14 @@
+package chapter11;
+
+import java.util.*;
+
+public class ch11_28 {
+    public static void main(String[] args) {
+
+        char ch=' ';
+        for (int i=0;i<95;i++)
+            System.out.print(ch++);
+
+
+    }
+}

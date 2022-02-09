ef1a14f1317bb4e504077895d15c2238301e96d7
kyungmin <nkm4095@naver.com>
2022-02-09 16:24:26 +0900

    ch12

--- src/chapter12/ch12_08.java
+++ src/chapter12/ch12_08.java
@@ -4,32 +4,66 @@ import java.util.EmptyStackException;
 import java.util.Vector;
 
 public class ch12_08 extends Vector {
-    public Object push(Object item){
-        addElement(item);
-        return item;
-    }
-    public Object pop(){
-        Object obj=peek();
-        removeElementAt(size()-1);
-        return obj;
-    }
-    public Object peek(){
-        int len=size();
+    public static void main(String[] args) {
 
-        if (len==0){
-            throw new EmptyStackException();
-        }
-        return elementAt(len-1);
-    }
-    public Object empty(){
-        return size()==0;
-    }
-    public  Object search(Object o ){
-        int i=lastIndexOf(o);
 
-        if (i>0){
-            return size()-1;
-        }
-        return -1;
+    MyTransportation t1 = MyTransportation.BUS;
+    MyTransportation t2 = MyTransportation.BUS;
+    MyTransportation t3 = MyTransportation.TRAIN;
+    MyTransportation t4 = MyTransportation.SHIP;
+    MyTransportation t5 = MyTransportation.AIRPLANE;
+
+        System.out.printf("t1=%s, %d%n", t1.name(), t1.ordinal());
+        System.out.printf("t2=%s, %d%n", t2.name(), t2.ordinal());
+        System.out.printf("t3=%s, %d%n", t3.name(), t3.ordinal());
+        System.out.printf("t4=%s, %d%n", t4.name(), t4.ordinal());
+        System.out.printf("t5=%s, %d%n", t5.name(), t5.ordinal());
+        System.out.println("t1==t2 ? "+(t1==t2));
+        System.out.println("t1.compareTo(t3)="+ t1.compareTo(t3));
+}
+}
+abstract class MyEnum<T extends MyEnum<T>> implements Comparable<T> {
+    static int id = 0;
+
+    int ordinal;
+    String name = "";
+
+    public int ordinal() { return ordinal; }
+
+    MyEnum(String name) {
+        this.name = name;
+        ordinal = id++;
     }
+
+    public int compareTo(T t) {
+        return ordinal - t.ordinal();
+    }
+}
+
+abstract class MyTransportation extends MyEnum {
+    static final MyTransportation BUS   = new MyTransportation("BUS", 100) {
+        int fare(int distance) { return distance * BASIC_FARE; }
+    };
+    static final MyTransportation TRAIN = new MyTransportation("TRAIN", 150) {
+        int fare(int distance) { return distance * BASIC_FARE; }
+    };
+    static final MyTransportation SHIP  = new MyTransportation("SHIP", 100) {
+        int fare(int distance) { return distance * BASIC_FARE; }
+    };
+    static final MyTransportation AIRPLANE =
+            new MyTransportation("AIRPLANE", 300) {
+                int fare(int distance) { return distance * BASIC_FARE; }
+            };
+
+    abstract int fare(int distance); // Ãß»ó ¸Þ¼­µå
+
+    protected final int BASIC_FARE;
+
+    private MyTransportation(String name, int basicFare) {
+        super(name);
+        BASIC_FARE = basicFare;
+    }
+
+    public String name()     { return name; }
+    public String toString() { return name; }
 }

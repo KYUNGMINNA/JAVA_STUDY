247e7c980a449a5dc660813042038159339870ff
kyungmin <nkm4095@naver.com>
2022-02-11 16:56:35 +0900

    ch12

--- src/chapter12/ch12_06.java
+++ src/chapter12/ch12_06.java
@@ -8,7 +8,7 @@ public class ch12_06 {
     public static void main(String[] args) {
 
         for (Direction2 d: Direction2.values())
-            System.out.printf("%s = %d %n",d.name(),d.getValue());
+            System.out.printf("%s = %hd %n",d.name(),d.getValue());
 
         Direction2 d1=Direction2.EAST;
         Direction2 d2=Direction2.of(1);

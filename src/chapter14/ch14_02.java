f6fe3b2cd5a4ae89b15f898db22bf8516edafb66
kyungmin <nkm4095@naver.com>
2022-02-13 21:25:07 +0900

    ch14

---
+++ src/chapter14/ch14_02.java
@@ -0,0 +1,24 @@
+package chapter14;
+
+public class ch14_02 {
+
+    public static void main(String[] args) {
+
+
+        MyFunction2 f1 = () -> {};
+        Object obj = (MyFunction2) (() -> {});
+        String str = ((Object) (MyFunction2) (() -> {})).toString();
+
+        System.out.println(f1);
+        System.out.println(obj);
+        System.out.println(str);
+
+        System.out.println((MyFunction2)()->{});
+        System.out.println((Object) (MyFunction2)()->{});
+
+    }
+}
+@FunctionalInterface
+interface MyFunction2{
+    public  abstract void myMethod();
+}

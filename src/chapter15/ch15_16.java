cc2c5622cdc2e50a9af88dabafdad80775e6b66e
kyungmin <nkm4095@naver.com>
2022-02-19 14:00:08 +0900

    ch15

--- src/chapter15/ch15_16.java
+++ src/chapter15/ch15_16.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class ch15_16 {
     public static void main(String args[]) {
         try {
-            String fileName = "test.txt";
+            String fileName = "s.txt";
             FileInputStream fis = new FileInputStream(fileName);
             FileReader	    fr  = new FileReader(fileName);
 
---
+++ src/chapter15/ch15_24.java
@@ -0,0 +1,20 @@
+package chapter15;
+
+import java.io.*;
+
+public class ch15_24 {
+    public static void main(String[] args) {
+       PrintStream ps=null;
+       FileOutputStream fos=null;
+
+       try{
+           fos=new FileOutputStream("C:\\Users\\kyungmin\\IdeaProjects\\JAVA_STUDY\\src\\chapter15/test.txt");
+           ps=new PrintStream(fos);
+           System.setOut(ps);
+       }catch (FileNotFoundException e){
+           System.err.println("File Not Found.");
+       }
+        System.out.println("hello by System.out");
+        System.err.println("heelo by System.err");
+    }
+}

cd7bc90fbf765040c257a0be034ac0659a6ca4da
kyungmin <nkm4095@naver.com>
2022-02-20 14:29:18 +0900

    ch15

---
+++ src/chapter15/ch15_39.java
@@ -0,0 +1,19 @@
+package chapter15;
+
+public class ch15_39 implements java.io.Serializable{
+    String name;
+    String password;
+    int age;
+
+    public ch15_39(){
+        this("Unkown","1111",0);
+    }
+    public ch15_39(String name,String password,int age){
+        this.name=name;
+        this.password=password;
+        this.age=age;
+    }
+    public String toString(){
+        return "("+name+","+password+","+age+")";
+    }
+}

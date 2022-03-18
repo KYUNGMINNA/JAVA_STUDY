c4e4a18b692a35bf2e3801020e0a4b027fb991c9
KYUNGMIN <62867182+KYUNGMINNA@users.noreply.github.com>
2022-03-18 10:13:05 +0900

    stu

--- src/stu/1weeks.java
+++ src/stu/1weeks.java
@@ -7,16 +7,21 @@ public class 1weeks{
     test[] te;
     
     b=new int[2];
+    b[0]=123;
+    b[1]=456;
+    
+    
     
     t=new t();
+    t.aa=1234567;
+    
     
     te=new test[2];
-    
+   
     te[0]=new t();
     te[1]=new t();
     
     te[0].aa=1;
-    
     te[1].aa=2;
     
     

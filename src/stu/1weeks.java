35b5b7950e4e13b52888a223fce1d13417a025bc
KYUNGMIN <62867182+KYUNGMINNA@users.noreply.github.com>
2022-03-18 10:11:02 +0900

    stu

---
+++ src/stu/1weeks.java
@@ -0,0 +1,29 @@
+public class 1weeks{ 
+  public static void main(String[] args){
+    int a;
+    int[] b;
+    
+    test t;
+    test[] te;
+    
+    b=new int[2];
+    
+    t=new t();
+    
+    te=new test[2];
+    
+    te[0]=new t();
+    te[1]=new t();
+    
+    te[0].aa=1;
+    
+    te[1].aa=2;
+    
+    
+    
+  }
+  
+}
+class test{ 
+  int aa;
+}

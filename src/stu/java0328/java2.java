4259f4ecffd6ae71a7e6fae9ec8eb38eb1656ccf
KYUNGMIN <62867182+KYUNGMINNA@users.noreply.github.com>
2022-03-31 13:27:07 +0900

    java study

---
+++ src/stu/java0328/java2.java
@@ -0,0 +1,29 @@
+package java0328;
+import java0401.B;
+
+public class java2{
+  B b=new B( ) ;
+  
+  b.b_a=3;  -- X
+  b.b_b=5;  -- O
+
+  b.b_methoda(); -- X
+  b.b_methodb(); -- O
+
+  B b2=new B(4); -- O 
+  
+}
+
+pacakage java0401;
+public class B{
+  int b_a;
+  
+  public int b_b;
+  
+  void b_methoda (  ){  }
+  
+  public void b_methodb ( ) { } 
+  
+  public B() { } 
+  public B(int c) { } 
+}

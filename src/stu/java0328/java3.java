315714c4def20d2aa7a45b97cb239e1df2ecb68e
KYUNGMIN <62867182+KYUNGMINNA@users.noreply.github.com>
2022-04-01 13:32:23 +0900

    java study

--- src/stu/java0328/java3.java
+++ src/stu/java0328/java3.java
@@ -10,17 +10,43 @@ class B implements A{
     System.out.println("Class B implements A");
   }
 }
-class C{
+class C implements A{
+  @Override
+  public void A_method() {
+    
+    System.out.println("Class C implements A");
+  }
+}
+class D{
   public static void main(String[] args){
     
     B b=new B();
     b.A_method();
     
+    C c=new C();
+    c.A_method();
     
     A a=new B();
     a.A_method();
+    
+    ///////////////////////////
+    
+    A aa;
+   
+    aa=b;
+    aa.A_method();
+    
+    aa=c;
+    aa.A_method();
   
-  
+  //////////  ::static A new_met( ) { } 
+    A aaa;
+    aaa=new_met();
+    aaa.a_method();
   }
-
+  static A new_met()
+  {
+    return new B;
+    //return new B;
+  }
 }

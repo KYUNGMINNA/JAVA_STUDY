dc230dd76bd98d0a71e2a9e0f5ded7b3bce6c0d8
kyungmin <nkm4095@naver.com>
2022-04-05 21:25:28 +0900

    java study

---
+++ src/stu/java0404/java.java
@@ -0,0 +1,25 @@
+package stu.java0404;
+
+public class java {
+    public static void main(String[] args) {
+
+        System.out.println(mehtod_B());
+    }
+    static int method_A(){
+        int a=1;
+        while(a<10){
+            a+=a;
+        }
+        return a;
+    }
+
+    static int mehtod_B(){
+        int d=0;
+        int b=3;
+        while(b<540){
+            int c=method_A();
+            b+=c;
+        }
+        return b;
+    }
+}

8aae1c7a0e5543949b4ee648cff63117827c0234
KYUNGMIN <62867182+KYUNGMINNA@users.noreply.github.com>
2022-04-23 21:45:21 +0900

    sql study

--- src/stu/db0421_db0422/8.주말숙제_0422.sql
+++ src/stu/db0421_db0422/8.주말숙제_0422.sql
@@ -1,3 +1,4 @@
+--문제1
 SELECT 
     job_id,
     AVG(salary)
@@ -5,14 +6,14 @@ FROM employees
 GROUP BY job_id
 ORDER BY AVG(salary) DESC;
 
+--문제2
 SELECT
     SUBSTR(hire_date,1,2),
     COUNT( SUBSTR(hire_date,1,2))
 FROM employees
 GROUP BY  SUBSTR(hire_date,1,2);
 
-SELECT * FROM employees;
-
+--문제3
 SELECT
     department_id,
     AVG(salary)
@@ -21,8 +22,7 @@ WHERE salary>=1000 --AND department_id IS NOT NULL
 GROUP BY department_id
 HAVING AVG(salary)>=2000 ;
 
-
-
+--문제4
 SELECT 
     department_id,
     SUM(salary+(salary*commission_pct)),

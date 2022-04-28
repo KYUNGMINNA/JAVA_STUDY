26766e1a254d3413cb1d09c7bb4f5ae7342796ed
KYUNGMIN <62867182+KYUNGMINNA@users.noreply.github.com>
2022-04-28 15:42:15 +0900

    sql study

---
+++ src/stu/db0425_db0429/14-1_시퀀스_index_0428.sql
@@ -0,0 +1,123 @@
+
+--시퀀스 (순차적으로 증가하는 값을 만들어 주는 객체)
+
+SELECT * FROM user_sequences;
+
+--시퀀스 생성  '테이블이름_seq' 로 짓는게 관례  | 괄호 사용 안함
+CREATE SEQUENCE dept2_seq
+    START WITH 1 --시작 값  |작성안할시 (기본값은 증가할 때는 최소값,감소할 때는 최대값)
+    INCREMENT BY 1 --증가 값 | (양수면 증가,음수면 감소  , --작성안할시 기본값1)
+    MAXVALUE 10 --최대 값 |작성안할시 (기본값 증가시 1027,감소일때 -1)
+    MINVALUE 1 --최소값 |작성안할시 (기본값 증가일 때 1, 감소일 때 -1028)
+    NOCACHE --캐시메모리 사용 여부 |작성안할시  CACHE :20 | 
+    NOCYCLE; --순환 여부 (1~10 이후 끝 or 다시 1부터 할것인지 ) 
+             --작성 안할시 NOCYCLE 기본,순환시키려면 CYCLE로
+             
+CREATE TABLE dept3(
+    dept_no NUMBER(2),
+    dept_name VARCHAR2(14),
+    loca VARCHAR2(13),
+    dept_date DATE
+); 
+
+RENAME dept2_seq TO dept3_seq;
+
+--시퀀스 사용하기 (NEXTVAL,CURRVAL)
+INSERT INTO dept3 VALUES(dept3_seq.NEXTVAL,'test','test',sysdate);
+
+SELECT * FROM dept3;
+
+--현재 시퀀스 번호 확인
+SELECT dept3_seq.CURRVAL FROM dual;
+
+
+--시퀀스 수정(직접 수정 가능)
+--START WITH은 수정이 불가능 합니다.->지우고 다시 만들어야 함 
+ALTER SEQUENCE dept3_seq MAXVALUE 9999; --최대 값 증가
+ALTER SEQUENCE dept3_seq INCREMENT BY -1; --증가 값 변경
+ALTER SEQUENCE dept3_seq MINVALUE 0; --최소 값 변경
+
+--SEQUENCE 초기화 방법  INCREMENT BY로 직접 바꾸기 ||||  SEQUENCE 삭제 후 재생성 
+--시퀀스 값을 다시 처음으로 돌리는 방법
+SELECT dept3_seq.CURRVAL FROM dual;
+
+--INCREMENT BY 변경으로 직접  SEQUENCE 직접 바꾸기
+SELECT dept3_seq.CURRVAL FROM dual;
+ALTER SEQUENCE dept3_seq INCREMENT BY -10;
+SELECT dept3_seq.NEXTVAL FROM dual;
+ALTER SEQUENCE dept3_seq INCREMENT BY 1;
+
+--시퀀스 지우기 
+DROP SEQUENCE dept3_seq;
+
+
+/*
+-index
+index는 PRIMARY KEY, UNIQUE 제약 조건에서 자동으로 생성되고 
+조회를 빠르게 해주는 hint 역할을 합니다.
+index는 조회를 빠르게 하지만, 무작위하게 많은 인덱스를 생성해서
+사용하면 오히려 성능 부하를 일으킬 수 있습니다.
+정말 필요할 때만 index를 사용하는 것이 올바릅니다.
+*/
+
+SELECT * FROM employees WHERE first_name='Nancy';
+
+--인덱스 추가  ::주로 많이 검색되는 컬럼에 인덱스를 만들면 정렬이 들어감 --검색 속도 빨라짐 
+--ORACLE은 PK를 설정하면 알아서 인덱스로 만들어 둠
+CREATE INDEX emp_first_name_idx ON employees(first_name);
+
+DROP INDEX emp_first_name_idx;
+/*
+- 인덱스가 권장되는 경우 
+1. 컬럼이 WHERE 또는 조인조건에서 자주 사용되는 경우
+2. 열이 광범위한 값을 포함하는 경우
+3. 테이블이 대형인 경우
+4. 타겟 컬럼이 많은 수의 null값을 포함하는 경우.
+5. 테이블이 자주 수정되고, 이미 하나 이상의 인덱스를 가지고 있는 경우에는
+ 권장하지 않습니다.
+*/
+
+--시퀀스와 인덱스를 사용하는 hint 방법.
+CREATE SEQUENCE board_seq
+    START WITH 1
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;
+
+CREATE TABLE tbl_board(
+    bno NUMBER(10) PRIMARY KEY,
+    writer VARCHAR2(20)
+);
+
+INSERT INTO tbl_board 
+VALUES(board_seq.NEXTVAL,'test');
+
+INSERT INTO tbl_board 
+VALUES(board_seq.NEXTVAL,'admin');
+
+INSERT INTO tbl_board 
+VALUES(board_seq.NEXTVAL,'hong');
+
+INSERT INTO tbl_board 
+VALUES(board_seq.NEXTVAL,'kim');
+
+
+SELECT* FROM tbl_board;
+
+--인덱스 이름 변경
+ALTER INDEX SYS_C007063 
+RENAME TO tbl_board_idx;
+
+
+-- /*+ INDEX_DESC(table_name  index_name)*/
+-- :지정된 인덱스를 강제로 쓰게끔 지정 
+--INDEX  ASC ,DESC를 추가해서 내림차,오름차 순으로 쓰게끔 지정도 가능.
+SELECT * FROM
+    (
+    SELECT /*+ INDEX_DESC(tbl_board tbl_board_idx)*/    
+        ROWNUM AS rn,
+        bno,
+        writer
+    FROM tbl_board
+    )
+WHERE rn>10 AND rn<=20;

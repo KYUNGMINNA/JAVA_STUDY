9b5205931706145566c1f9e4fe69d2b13f1f3ca7
KYUNGMIN <62867182+KYUNGMINNA@users.noreply.github.com>
2022-05-12 17:39:19 +0900

    sql study

---
+++ src/stu/jsp0506_/0512/jdbcBasic/create_table.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE scores(
+    id NUMBER PRIMARY KEY,
+    name VARCHAR2(30) NOT NULL,
+    kor NUMBER(3) DEFAULT 0,
+    eng NUMBER(3) DEFAULT 0,
+    math NUMBER(3) DEFAULT 0,
+    total NUMBER(3) DEFAULT 0,
+    average NUMBER(5,2)
+);
+
+CREATE SEQUENCE id_seq 
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 1000
+    NOCACHE
+    NOCYCLE;
+
+
+select * FROM scores;

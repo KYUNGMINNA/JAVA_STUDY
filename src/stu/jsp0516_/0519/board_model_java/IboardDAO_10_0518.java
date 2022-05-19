f6ad513971b4c17f49543b167ee30028e9ce3679
KYUNGMIN <62867182+KYUNGMINNA@users.noreply.github.com>
2022-05-19 17:49:09 +0900

    java&jsp study

---
+++ src/stu/jsp0516_/0519/board_model_java/IboardDAO_10_0518.java
@@ -0,0 +1,31 @@
+package kr.co.jsp.board.model;
+
+import java.util.List;
+
+public interface IBoardDAO_10_0518 {
+	
+	//글 등록 메서드 
+//	void regist(BoardVO_9_0518 vo); -- 파라미터 객체로 작성해도 됨 
+	void regist(String writer,String title,String content);
+	
+	//글 전체 목록 가지고 오는 메서드
+	List<BoardVO_9_0518> listBoard();
+	
+	//글 상세보기 요청을 처리할 메서드
+	BoardVO_9_0518 contentBoard(int bId);
+	
+	
+	//글 수정 요청을 처리할 메서드(제목,내용)  
+//	void updateBoard(BoardVO_9_0518 vo); --객체도 가능 
+	void updateBoard(String title,String content,int bId);
+	
+	
+	//글 삭제 요청을 처리할 메서드
+	void deleteBoard(int bId);
+	
+	//글 검색 요청을 처리할 메서드
+	List<BoardVO_9_0518> searchBoard(String keyword,String category);
+	
+	//조회수를 올려주는 메서드 :UPDATE my_board SET hit=hit+1 WHERE board_id=?
+	void upHit(int bId);
+}

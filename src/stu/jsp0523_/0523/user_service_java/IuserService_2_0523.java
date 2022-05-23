a9c9dbee50071d73f6169d6e5ccc6b21f3236f5f
KYUNGMIN <62867182+KYUNGMINNA@users.noreply.github.com>
2022-05-23 15:45:45 +0900

    java&jsp study

---
+++ src/stu/jsp0523_/0523/user_service_java/IuserService_2_0523.java
@@ -0,0 +1,10 @@
+package kr.co.jsp.user.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface IUserService_2_0523 {
+
+	void execute(HttpServletRequest request, HttpServletResponse response);
+
+}
